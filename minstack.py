444ff4ab510c6182302d66a86995d9ef324f911b
Vysakh-T <53861783+Vysakh-T@users.noreply.github.com>
2020-09-24 12:19:31 +0530

    Solution to 155. Min Stack

---
+++ minstack.py
@@ -0,0 +1,40 @@
+class MinStack(object):
+    
+    s = []
+    
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.s = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.s.insert(0,x)
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.s=self.s[1:]
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.s[0]
+        
+        
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return min(self.s)
