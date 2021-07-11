230af329efcde8a422dda0751a2a6b6e741374fc
Vysakh-T <53861783+Vysakh-T@users.noreply.github.com>
2021-07-11 14:25:57 +0530

    "Added Git Ignore"

--- temp.py
+++ temp.py
@@ -1,18 +1,31 @@
-def moveZeroes(nums):
-    """
-    :type nums: List[int]
-    :rtype: None Do not return anything, modify nums in-place instead.
-    """
-    j = 0
-    count = 1
-    while (j<len(nums)):
-        if(count==len(nums)):
-            return
-        if nums[j]==0:
-            nums.pop(j)
-            nums.append(0)
-            j-=1
-        j+=1
-        count+=1
+T = int(input())
+for t in range(T):
+    N,M = map(int,input().split())
+    P = {}
+    O = ""
+    for n in range(N):
+        t1,t2 = map(int,input().split())
+        temp = list(range(t1,t2+1))
+        for i in temp:
+            P[i] = 1
+    M = list(map(int,input().split()))
     
+    for m in range(len(M)):
+        if P.get(M[m]) != None:
+            O += str(M[m]) + " "
+            P[M[m]] = None
+        else:
+            i = 1
+            while(True):
+                if P.get(M[m]-i) != None and M[m]-i not in M[m+1:]:
+                    O += str(M[m]-i) + " "
+                    P[M[m]-i] = None
+                    break
+                elif P.get(M[m]+i) != None and M[m]+i not in M[m+1:]:
+                    O+= str(M[m]+i) + " "
+                    P[M[m]+1] = None
+                    break
+                i+=1
+    O = O[:len(O)-1]
+    print("Case #{x}: {y}".format(x=t+1,y=O))
     
